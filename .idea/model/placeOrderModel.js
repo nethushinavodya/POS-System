dc6a45573ed8521d8677e84b048fbc32e62ea9c5
nethushinavodya <nethushiperera@gmail.com>
2025-05-03 12:33:19 +0530

    POS

--- .idea/model/placeOrderModel.js
+++ .idea/model/placeOrderModel.js
@@ -9,4 +9,59 @@ export default class PlaceOrderModel {
         this._total = total;
     }
 
+    get orderId() {
+        return this._orderId;
+    }
+
+    set orderId(value) {
+        this._orderId = value;
+    }
+
+    get customerId() {
+        return this._customerId;
+    }
+
+    set customerId(value) {
+        this._customerId = value;
+    }
+
+    get itemId() {
+        return this._itemId;
+    }
+
+    set itemId(value) {
+        this._itemId = value;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(value) {
+        this._description = value;
+    }
+
+    get unitPrice() {
+        return this._unitPrice;
+    }
+
+    set unitPrice(value) {
+        this._unitPrice = value;
+    }
+
+    get quantity() {
+        return this._quantity;
+    }
+
+    set quantity(value) {
+        this._quantity = value;
+    }
+
+    get total() {
+        return this._total;
+    }
+
+    set total(value) {
+        this._total = value;
+    }
 }
