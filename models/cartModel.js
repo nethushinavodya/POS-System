3859ea520dc41c9bc33af6ac6851c262d945f52a
nethushinavodya <nethushiperera@gmail.com>
2025-05-04 11:54:29 +0530

    POS

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,6 +1,7 @@
 export default class CartModel {
     constructor(orderId, customerId, itemId, description, unitPrice, quantity, total) {
         this._orderId = orderId;
+        this._date = new Date();
         this._customerId = customerId;
         this._itemId = itemId;
         this._description = description;
--- models/orderDetailsModel.js
+++ models/orderDetailsModel.js
@@ -1,7 +1,7 @@
 export default class orderDetailsModel {
     constructor(orderId, date, customerId, quantity, total, status) {
         this._orderId = orderId;
-        this._date = date;
+        this._date = new Date();
         this._customerId = customerId;
         this._quantity = quantity;
         this._total = total;
